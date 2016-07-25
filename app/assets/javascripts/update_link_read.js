8a8fc1c9d9ff152a26ba89832703ba9cd2be8428
Jbern16 <jbern16@gmail.com>
2016-07-25 14:10:42 -0600

    add ajax call for adding striekthrough and update status

---
+++ app/assets/javascripts/update_link_read.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $(".links").on("click", ".read-box", function(){
+    var id = $(this).parent().parent().data("id")
+    $.ajax({
+      type: "PATCH",
+      url: "/api/v1/links/" + id , 
+      data: { "link": {"read": "true"} },
+      dataType: "JSON",
+      success: function(response) {
+        $("#link-" + id).css("text-decoration", "line-through")
+      }
+    })
+  })
+})
