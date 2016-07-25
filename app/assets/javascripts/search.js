e2ae048b08d7a7bc0977999b1440dafa4c3cdc60
Jbern16 <jbern16@gmail.com>
2016-07-25 15:53:22 -0600

    search function is finished

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,24 +1,24 @@
 $(document).ready(function(){
   $("#search").on("keyup", function(){
     var word = $(this).val().toLowerCase()
-    var title_and_urls = $(".links").children("ul").children(".link-title", ".link-url").map(function(){
+    var title_and_urls = $(".links").children("ul").children().map(function(){
       return $(this)
     })
 
     var not_matched = title_and_urls.filter(function(index, element){
-      return !$(element).text().toLowerCase.includs(word)
+      return !$(element).text().toLowerCase().includes(word)
     })
     
     var matched = title_and_urls.filter(function(index, element){
-      return $(element).text().toLowerCase.includs(word)
+      return $(element).text().toLowerCase().includes(word)
     })
 
     $.each(not_matched, function(index, element){
-      $(element).parent.hide()
+      $(element).parent().hide()
     })
 
     $.each(matched, function(index, element){
-      $(element).parent.hide()
+      $(element).parent().show()
     })
   })
 })
